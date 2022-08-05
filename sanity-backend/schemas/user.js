ca724781b5b9aa4890f4e416c5c10bf34897e595
deepaksingh8287 <deepaksingh33june@gmail.com>
2022-08-05 20:56:14 +0530

    first commit

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name:"user",
+    title:"user",
+    type:"document",
+    fields:[
+        {
+            name:"userName",
+            title:"user name",
+            type:"string"
+        },
+        {
+            name:"image",
+            title:"image",
+            type:"string"
+        },
+    ]
+}
